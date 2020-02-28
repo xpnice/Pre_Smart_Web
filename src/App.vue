dbfbc17752a8ef9e1f66c57162f91831580ccad8
LiYanPeng <xp970805@gmail.com>
2020-02-28 23:19:26 +0800

    20190228

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
     <img src="./assets/logo.png" />
     <h1>{{ msg }}</h1>
     <h1>{{  details()}}</h1>
+    <pre><a v-bind:href="url">我的博客 </a></pre>
+    <input v-model="msg">
+    <button v-on:click="onclick">Click Me</button>
+    <h1 v-if="ok">你好</h1>
     <!--<router-view />-->
   </div>
 </template>
@@ -13,12 +17,18 @@ export default {
   data () {
     return {
       msg: 'hello',
-      name: '李骏垚'
+      name: '李骏垚',
+      url: 'www.lyp970805.com',
+      ok: true
     }
   },
   methods: {
     details: function () {
-      return this.name + '能不能别往外跑了'
+      return (this.ok ? '' : '超帅的') + this.name + '能不能别往外跑了'
+    },
+    onclick: function () {
+      this.name = this.name === '李沿澎' ? '李骏垚' : '李沿澎'
+      this.ok = !this.ok
     }
   }
 }
